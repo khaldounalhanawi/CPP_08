f3e47ff10d2dda7bd7c79dd3fc26cca7ef579e0c
Khaldoun Alhanawi <khaldounalhanawi@users.noreply.github.com>
2026-05-12 10:20:21 +0200

    ex02: done

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,17 @@
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	private:
+		// nix
+
+	public:
+		typedef typename std::stack<T>::container_type::iterator		iterator;
+		typedef typename std::stack<T>::container_type::const_iterator	const_iterator;
+
+		iterator		begin()			{ return (this->c.begin()); };
+		const_iterator	begin() const	{ return (this->c.begin()); };
+		iterator		end()			{ return (this->c.end()); };
+		const_iterator	end() const		{ return (this->c.end()); };
+};
